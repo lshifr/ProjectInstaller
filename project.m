bb5f088026ceeab45c6c29f1779d665a60589200
lshifr <lshifr@gmail.com>
2012-11-25 23:38:53 +0300

    Add project.m project descriptor

---
+++ project.m
@@ -0,0 +1,15 @@
+{
+        "author" ->
+           {
+               "name" -> "Leonid Shifrin",
+               "email" -> "lshifr@gmail.com",
+               "url" -> "http://www.mathprogramming-intro.org"
+           },           
+        "name" -> "ProjectInstaller",
+        "version" -> "1.0",
+        "mathematica_version" -> "8.0+",
+        "dependencies" -> {},
+        "description"-> "Simple web installer for Mathematica projects",
+        "url" -> "https://github.com/lshifr/ProjectInstaller",
+        "license" ->  "MIT"
+}
